017a237e5defb65967a3446bb9fecfe6a9e3ec4c
valdomirocm2016 <valdomirocm@gmail.com>
2019-09-21 20:55:04 -0300

    User basic validation - Bean Validation

--- src/main/java/com/valdomiro/curso/dto/UserInsertDTO.java
+++ src/main/java/com/valdomiro/curso/dto/UserInsertDTO.java
@@ -2,17 +2,32 @@ package com.valdomiro.curso.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.valdomiro.curso.entities.User;
 
 public class UserInsertDTO implements Serializable{
 	  /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	private Long id;
+	  private static final long serialVersionUID = 1L;
+	  private Long id;
+	  @NotEmpty(message=" can't be empty")
+	  @Length(min=5, max=80, message="length must be beteween 5 and 80")
 	  private String name;
+	  
+	  @NotEmpty(message=" can't be empty")
+	  @Email(message="invalid email")
 	  private String email;
+	  
+	  @NotEmpty(message=" can't be empty")
+	  @Length(min=8, max=20, message="length must be beteween 8 and 20")
 	  private String phone;
+	  
+	  @NotEmpty(message=" can't be empty")
 	  private String password;
 	  
 	  public UserInsertDTO() {
--- src/main/java/com/valdomiro/curso/resources/UserResource.java
+++ src/main/java/com/valdomiro/curso/resources/UserResource.java
@@ -3,6 +3,8 @@ package com.valdomiro.curso.resources;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,7 +42,7 @@ public class UserResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto){
 		
 		UserDTO newDto= service.insert(dto);
 		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
