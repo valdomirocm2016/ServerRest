d4e886b19ac071cb9eb7417877a42b1703e7bcf2
valdomirocm2016 <valdomirocm@gmail.com>
2019-09-21 15:47:24 -0300

    Product Insert

--- src/main/java/com/valdomiro/curso/resources/ProductResource.java
+++ src/main/java/com/valdomiro/curso/resources/ProductResource.java
@@ -1,15 +1,22 @@
 package com.valdomiro.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.valdomiro.curso.dto.ProductCategoriesDTO;
 import com.valdomiro.curso.dto.ProductDTO;
+import com.valdomiro.curso.dto.UserDTO;
+import com.valdomiro.curso.dto.UserInsertDTO;
 import com.valdomiro.curso.entities.Product;
 import com.valdomiro.curso.services.ProductService;
 
@@ -32,5 +39,13 @@ public class ProductResource {
 		ProductDTO dto= service.findById(id);
 		return ResponseEntity.ok().body(dto);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto){
+		
+		ProductDTO newDto= service.insert(dto);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
 	}
 }
--- src/main/java/com/valdomiro/curso/services/ProductService.java
+++ src/main/java/com/valdomiro/curso/services/ProductService.java
@@ -6,11 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.valdomiro.curso.dto.CategoryDTO;
+import com.valdomiro.curso.dto.ProductCategoriesDTO;
 import com.valdomiro.curso.dto.ProductDTO;
-import com.valdomiro.curso.dto.UserDTO;
+import com.valdomiro.curso.entities.Category;
 import com.valdomiro.curso.entities.Product;
-import com.valdomiro.curso.entities.User;
+import com.valdomiro.curso.repositories.CategoryRepository;
 import com.valdomiro.curso.repositories.ProductRepository;
 import com.valdomiro.curso.services.exceptions.ResourceNotFoundException;
 
@@ -20,6 +23,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	public List<ProductDTO> findAll(){
 		List<Product> list= repository.findAll();
@@ -33,5 +39,23 @@ public class ProductService {
 		Optional<Product> obj= repository.findById(id);
 		Product entity= obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity= dto.toEntity();
+		setProductCategories(entity,dto.getCategories());
+		entity= repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		
+		for(CategoryDTO dto: categories) {
+			Category category= categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
+		
 	}
 }
